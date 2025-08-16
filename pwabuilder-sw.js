e1b8a12c182a4db029cf5779ec0c2acd6c6de28b
Brianbru2022 <117004161+Brianbru2022@users.noreply.github.com>
2025-08-16 18:36:20 +0100

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,21 +1,47 @@
-// Basic offline service worker for your game
-const CACHE_NAME = "ochil-giant-cache-v1";
-const offlineFallbackPage = "offline.html";
+// This is the "Offline page" service worker
 
-self.addEventListener("install", event => {
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll([offlineFallbackPage]))
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request).then(resp => {
-      return resp || (event.request.mode === "navigate"
-        ? caches.match(offlineFallbackPage)
-        : undefined);
-    }))
-  );
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
